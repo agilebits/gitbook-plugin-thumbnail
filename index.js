a0a92661274762e88c4d91815311b1dda737bdce
Mitchell Cohen <mitchell@agilebits.com>
2016-01-13 13:37:22 +0100

    Removed AG-specific features

--- index.js
+++ index.js
@@ -4,11 +4,8 @@ module.exports = {
         html: {
             "head:start": function () {
                 var path = this.book.options.pluginsConfig.thumbnail || "cover_small.png";
-                var icons = "<link rel='shortcut icon' type='image/x-icon' href='https://static.1password.com/favicon.ico'><link rel='apple-touch-icon-precomposed' href='/images/apple-touch-icon.png'><link rel='apple-touch-icon-precomposed' sizes='152x152' href='/images/apple-touch-icon.png'>";
                 return "<meta name='thumbnail' content='"+path+"'>"+icons;
-            },
-            "head:end": "<link rel='shortcut icon' type='image/x-icon' href='https://static.1password.com/favicon.ico'><link rel='apple-touch-icon-precomposed' href='/images/apple-touch-icon.png'><link rel='apple-touch-icon-precomposed' sizes='152x152' href='/images/apple-touch-icon.png'>",
-            "body:end": "<script src='/js/ug-history.js'></script>"
+            }
         }
     }
 };
